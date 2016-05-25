01e7193d5af64f7439a33e229c82ae5bb27c73a3
Adam Clemons <adam@adamclmns.com>
2016-05-24 19:44:42 -0500

    Removing binaries

--- src/dist/testScript.sh
+++
@@ -1,8 +0,0 @@
-TARGET_DIR='/home/pi/'
-for directory in ${TARGET_DIR}*;
-do
-  if [ -f "$directory" ] ; then
-    size=`ls -l $directory |cut -d" " -f5`
-    echo " ${directory##*/} --- $size"
-  fi
-done
